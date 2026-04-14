e6f1552bf2de59f161d6814cd5d2deb0f4cc9368
jakobpl <z2039007@students.niu.edu>
2026-04-14 13:26:42 -0500

    Initial Commit

---
+++ gemini-shortcut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gemini-shortcut
+//
+//  Created by jakob n on 4/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gemini-shortcut/gemini_shortcutApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemini_shortcutApp.swift
+//  gemini-shortcut
+//
+//  Created by jakob n on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct gemini_shortcutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
